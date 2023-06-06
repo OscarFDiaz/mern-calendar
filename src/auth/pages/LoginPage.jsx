df64a11e8754a6a89922a99bb7389e759c3b458d
Oscar <chikenlitle99@hotmail.com>
2023-06-05 22:46:00 -0600

    Form styled

--- src/auth/pages/LoginPage.jsx
+++ src/auth/pages/LoginPage.jsx
@@ -7,14 +7,20 @@ export const LoginPage = () => {
         <div className="col-md-6 login-form-1">
           <h3>Ingreso</h3>
           <form>
-            <div className="form-group mb-2">
+            <div className="form-group mb-2 form-floating">
               <input type="text" className="form-control" placeholder="Correo" />
+              <label>Correo</label>
             </div>
-            <div className="form-group mb-2">
+            <div className="form-group mb-4 form-floating">
               <input type="password" className="form-control" placeholder="Contraseña" />
+              <label>Contraseña</label>
             </div>
-            <div className="form-group mb-2">
-              <input type="submit" className="btnSubmit" value="Login" />
+            <div className="form-group mb-2 text-center">
+              <input
+                type="submit"
+                className="btnSubmit rounded-5 p-3 fs-5"
+                value="Login"
+              />
             </div>
           </form>
         </div>
@@ -22,26 +28,34 @@ export const LoginPage = () => {
         <div className="col-md-6 login-form-2">
           <h3>Registro</h3>
           <form>
-            <div className="form-group mb-2">
+            <div className="form-group mb-2 form-floating">
               <input type="text" className="form-control" placeholder="Nombre" />
+              <label>Nombre</label>
             </div>
-            <div className="form-group mb-2">
+            <div className="form-group mb-2 form-floating">
               <input type="email" className="form-control" placeholder="Correo" />
+              <label>Correo</label>
             </div>
-            <div className="form-group mb-2">
+            <div className="form-group mb-2 form-floating">
               <input type="password" className="form-control" placeholder="Contraseña" />
+              <label>Contraseña</label>
             </div>
 
-            <div className="form-group mb-2">
+            <div className="form-group mb-4 form-floating">
               <input
                 type="password"
                 className="form-control"
                 placeholder="Repita la contraseña"
               />
+              <label>Repita la contraseña</label>
             </div>
 
-            <div className="form-group mb-2">
-              <input type="submit" className="btnSubmit" value="Crear cuenta" />
+            <div className="form-group mb-2 text-center">
+              <input
+                type="submit"
+                className="btnSubmit rounded-5 p-3 fs-5"
+                value="Crear cuenta"
+              />
             </div>
           </form>
         </div>
